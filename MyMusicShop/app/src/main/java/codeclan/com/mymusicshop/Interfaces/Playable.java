359c4f02d3219b57f81788bcd7cf43ffddc3b211
Joao <johnynecas@hotmail.com>
2017-11-03 13:38:13 +0000

    created project and package structure and first class files

---
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Interfaces/Playable.java
@@ -0,0 +1,8 @@
+package codeclan.com.mymusicshop.Interfaces;
+
+/**
+ * Created by user on 03/11/2017.
+ */
+
+public interface Playable {
+}
---
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/ShopObject.java
@@ -0,0 +1,8 @@
+package codeclan.com.mymusicshop.Shopcontent;
+
+/**
+ * Created by user on 03/11/2017.
+ */
+
+public abstract class ShopObject {
+}
