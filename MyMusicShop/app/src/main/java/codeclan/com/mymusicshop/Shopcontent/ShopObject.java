9c8e5abfa098d73dd4aa2173a96eb62a999843f5
Joao <johnynecas@hotmail.com>
2017-11-03 13:57:59 +0000

    created interface playable and shopObject class

--- MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/ShopObject.java
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/ShopObject.java
@@ -1,8 +1,20 @@
 package codeclan.com.mymusicshop.Shopcontent;
 
+import codeclan.com.mymusicshop.Interfaces.Sellable;
+
 /**
  * Created by user on 03/11/2017.
  */
 
-public abstract class ShopObject {
+public abstract class ShopObject implements Sellable {
+     Double buyPrice;
+     Double sellPrice;
+
+
+    public ShopObject( Double buyPrice, Double sellPrice){
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        
+
+    }
 }
