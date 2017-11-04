8664086ecb96f685f9be2ea76ff9d363e8b58e34
Joao <johnynecas@hotmail.com>
2017-11-04 20:57:34 +0000

    created Strings class

--- MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/Strings.java
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/Strings.java
@@ -4,5 +4,16 @@ package codeclan.com.mymusicshop.Shopcontent;
  * Created by user on 03/11/2017.
  */
 
-public class Strings {
+public class Strings extends Instruments {
+
+     private String type;
+     private String name;
+     private  int numberOfStrings;
+
+    public Strings( String type, String name, int numberOfStrings String colour, String material, Double buyPrice, Double sellPrice){
+        super(colour,  material,  buyPrice, sellPrice);
+        this.type = type;
+        this.name = name;
+        
+    }
 }
