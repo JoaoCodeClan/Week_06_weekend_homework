a70b16d5bedd6b32979de670c62d560d40ee10a9
Joao <johnynecas@hotmail.com>
2017-11-04 22:39:37 +0000

    created shop class

--- MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shop.java
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shop.java
@@ -1,8 +1,27 @@
 package codeclan.com.mymusicshop;
 
+import java.util.ArrayList;
+
+import codeclan.com.mymusicshop.Interfaces.Sellable;
+
 /**
  * Created by user on 03/11/2017.
  */
 
 public class Shop {
+
+
+    private String shopName;
+    private ArrayList<Sellable>stock;
+
+
+    public Shop(String shopName, ArrayList<Sellable> stock){
+
+        this.shopName = shopName;
+        this.stock = stock;
+
+    }
+
+    
+
 }
