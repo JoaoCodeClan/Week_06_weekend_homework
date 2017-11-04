410f9e2b94c9676c43697963836a56ed287e0c63
Joao <johnynecas@hotmail.com>
2017-11-04 22:24:27 +0000

    created Enum Intensity to use in percursion and changed properties accordingly

---
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/Intensity.java
@@ -0,0 +1,14 @@
+package codeclan.com.mymusicshop.Shopcontent;
+
+/**
+ * Created by user on 04/11/2017.
+ */
+
+enum Intensity {
+
+    VERYLOUD,
+    LOUD,
+    MEDIUM,
+    LIGHT
+
+}
--- MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/Percursion.java
+++ MyMusicShop/app/src/main/java/codeclan/com/mymusicshop/Shopcontent/Percursion.java
@@ -8,14 +8,43 @@ public class Percursion extends Instruments {
 
     private String type;
     private String name;
-    private String intensity;
+    private Intensity intensity;
 
-    public Percursion( String type, String name, String intensity, String colour, String material, Double buyPrice, Double sellPrice){
+    public Percursion( String type, String name, Intensity intensity, String colour, String material, Double buyPrice, Double sellPrice){
         super(colour, material,buyPrice, sellPrice);
         this.type = type;
         this.name = name;
         this.intensity= intensity;
 
     }
+
+    public String play() {
+        return super.play() + "playing a " + this.type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String newType) {
+        this.type = newType;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    public Intensity getIntensity() {
+        return this.intensity;
+    }
+
+    public void setintensity(Intensity newIntensity) {
+        this.intensity = newIntensity;
+    }
+
 
 }
